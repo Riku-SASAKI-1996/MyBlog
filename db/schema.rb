e03de83abfcac86a26fbf7bd80c8bd0b2cf6c3dc
Riku SASAKI <kirusakiasa@gmail.com>
2017-10-05 14:15:42 +0000

    Add time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004160441) do
+ActiveRecord::Schema.define(version: 20171005141351) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171004160441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "time"
   end
 
 end
